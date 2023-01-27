139348ff5dc1bd5aa2e5a7140c895a0130e19234
Cl84Dev <cl84dev@yandex.com>
2023-01-27 12:11:20 -0300

    Implement getting data from an API

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import quotes from "./data/data";
+import axios from "axios";
 
 const App = () => {
-  const [random, setRandom] = useState(Math.floor(Math.random() * 5));
+  const [quotes, setQuotes] = useState([
+    {
+      quote: "",
+      author: "",
+    },
+  ]);
+
+  const [randomColors, setRandomColors] = useState(
+    Math.floor(Math.random() * 5)
+  );
+  const [randomQuotes, setRandomQuotes] = useState(
+    Math.floor(Math.random() * quotes.length)
+  );
+
+  useEffect(() => {
+    axios
+      .get("https://random-quote-back-end-production.up.railway.app/quote")
+      .catch((err) => console.log(err))
+      .then((res) => setQuotes(res.data));
+  }, []);
 
   const changeQuote = () => {
-    setRandom(() => Math.floor(Math.random() * 10));
+    setRandomColors(() => Math.floor(Math.random() * 5));
+    setRandomQuotes(() => Math.floor(Math.random() * quotes.length));
   };
 
-  const twitter = `https://twitter.com/intent/tweet?hashtags=quotes&text=%22${quotes[random].quote}%22%0A${quotes[random].author}`;
-  const whatsapp = `https://api.whatsapp.com/send?text=%22${quotes[random].quote}%22%0A${quotes[random].author}`;
+  const twitter = `https://twitter.com/intent/tweet?hashtags=quotes&text=%22${quotes[randomQuotes].quotation}%22%0A${quotes[randomQuotes].author}`;
+  const whatsapp = `https://api.whatsapp.com/send?text=%22${quotes[randomQuotes].quotation}%22%0A${quotes[randomQuotes].author}`;
 
-  const textColors = [
-    "#0d6efd",
-    "#6c757d",
-    "#198754",
-    "#dc3545",
-    "#212529",
-    "#0d6efd",
-    "#6c757d",
-    "#198754",
-    "#dc3545",
-    "#212529",
-  ];
+  const textColors = ["#0d6efd", "#6c757d", "#198754", "#dc3545", "#212529"];
 
   const buttonClasses = [
     "btn btn-primary",
@@ -32,15 +41,10 @@ const App = () => {
     "btn btn-success",
     "btn btn-danger",
     "btn btn-dark",
-    "btn btn-primary",
-    "btn btn-secondary",
-    "btn btn-success",
-    "btn btn-danger",
-    "btn btn-dark",
   ];
 
-  document.body.style.backgroundColor = textColors[random];
-  document.body.style.color = textColors[random];
+  document.body.style.backgroundColor = textColors[randomColors];
+  document.body.style.color = textColors[randomColors];
 
   return (
     <div className="d-flex justify-content-center m-3 mt-5">
@@ -51,12 +55,12 @@ const App = () => {
         <div className="d-flex p-3">
           <i className="bi bi-quote fs-1"></i>
           <p className="fs-5 p-3" id="text">
-            {quotes[random].quote}
+            {quotes[randomQuotes].quotation}
           </p>
         </div>
 
         <p className="fs-6" id="author">
-          {quotes[random].author}
+          {quotes[randomQuotes].author}
         </p>
 
         <div className="d-flex justify-content-around align-items-center p-3">
@@ -66,7 +70,7 @@ const App = () => {
               target="_blank"
               title="Share this quote on WhatsApp!"
             >
-              <i class="bi bi-whatsapp fs-1 px-3"></i>
+              <i className="bi bi-whatsapp fs-1 px-3"></i>
             </a>
 
             <a
@@ -76,12 +80,12 @@ const App = () => {
               target="_blank"
               title="Tweet this quote!"
             >
-              <i class="bi bi-twitter fs-1 px-3"></i>
+              <i className="bi bi-twitter fs-1 px-3"></i>
             </a>
           </div>
 
           <button
-            className={buttonClasses[random]}
+            className={buttonClasses[randomColors]}
             id="new-quote"
             onClick={changeQuote}
           >
