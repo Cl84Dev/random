cdf021db6bbf5d42547e6635283755d8abaecee0
Cl84Dev <cl84dev@yandex.com>
2023-06-30 10:09:35 -0300

    Change randomQuotes logic

--- src/App.js
+++ src/App.js
@@ -4,34 +4,27 @@ import "./App.css";
 import axios from "axios";
 
 const App = () => {
-  const [quotes, setQuotes] = useState([
-    {
-      quote: "",
-      author: "",
-    },
-  ]);
+  const [quotes, setQuotes] = useState(null);
 
   const [randomColors, setRandomColors] = useState(
     Math.floor(Math.random() * 5)
   );
-  const [randomQuotes, setRandomQuotes] = useState(
-    Math.floor(Math.random() * quotes.length)
-  );
+  const [randomQuotes, setRandomQuotes] = useState(null);
 
   useEffect(() => {
     axios
       .get("https://random-quote-back-end-production.up.railway.app/quote")
       .catch((err) => console.log(err))
-      .then((res) => setQuotes(res.data));
+      .then((res) => {
+        setRandomQuotes(Math.floor(Math.random() * res.data.length));
+        setQuotes(res.data);
+      });
   }, []);
 
   const changeQuote = () => {
     setRandomColors(() => Math.floor(Math.random() * 5));
     setRandomQuotes(() => Math.floor(Math.random() * quotes.length));
   };
-
-  const twitter = `https://twitter.com/intent/tweet?hashtags=quotes&text=%22${quotes[randomQuotes].quote}%22%0A${quotes[randomQuotes].author}`;
-  const whatsapp = `https://api.whatsapp.com/send?text=%22${quotes[randomQuotes].quote}%22%0A${quotes[randomQuotes].author}`;
 
   const textColors = ["#0d6efd", "#6c757d", "#198754", "#dc3545", "#212529"];
 
@@ -48,45 +41,48 @@ const App = () => {
 
   return (
     <div className="d-flex flex-column align-items-center justify-content-center m-3 mt-5">
-      <div className="quote-box bg-white text-center rounded-3 d-flex flex-column">
-        <div className="d-flex p-3">
-          <i className="bi bi-quote fs-1"></i>
-          <div className="d-flex flex-column justify-content-center fs-5 p-3 mx-auto">
-            {quotes[randomQuotes].quote ? (
+      {quotes && randomQuotes ? (
+        <div className="quote-box bg-white text-center rounded-3 d-flex flex-column">
+          <div className="d-flex p-3">
+            <i className="bi bi-quote fs-1"></i>
+            <div className="d-flex flex-column justify-content-center fs-5 p-3 mx-auto">
               <div>{quotes[randomQuotes].quote}</div>
-            ) : (
-              <div className="spinner-border" role="status"></div>
-            )}
-          </div>
-        </div>
-
-        <p className="fs-6">{quotes[randomQuotes].author}</p>
-
-        <div className="d-flex justify-content-around align-items-center p-3">
-          <div>
-            <a
-              href={whatsapp}
-              target="_blank"
-              title="Share this quote on WhatsApp!"
-            >
-              <i className="bi bi-whatsapp fs-1 px-3"></i>
-            </a>
-
-            <a
-              className="change-color"
-              href={twitter}
-              target="_blank"
-              title="Tweet this quote!"
-            >
-              <i className="bi bi-twitter fs-1 px-3"></i>
-            </a>
+            </div>
           </div>
 
-          <button className={buttonClasses[randomColors]} onClick={changeQuote}>
-            Change Quote
-          </button>
+          <p className="fs-6">{quotes[randomQuotes].author}</p>
+
+          <div className="d-flex justify-content-around align-items-center p-3">
+            <div>
+              <a
+                href={`https://api.whatsapp.com/send?text=%22${quotes[randomQuotes].quote}%22%0A${quotes[randomQuotes].author}`}
+                target="_blank"
+                title="Share this quote on WhatsApp!"
+              >
+                <i className="bi bi-whatsapp fs-1 px-3"></i>
+              </a>
+
+              <a
+                className="change-color"
+                href={`https://twitter.com/intent/tweet?hashtags=quotes&text=%22${quotes[randomQuotes].quote}%22%0A${quotes[randomQuotes].author}`}
+                target="_blank"
+                title="Tweet this quote!"
+              >
+                <i className="bi bi-twitter fs-1 px-3"></i>
+              </a>
+            </div>
+
+            <button
+              className={buttonClasses[randomColors]}
+              onClick={changeQuote}
+            >
+              Change Quote
+            </button>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="spinner-border" role="status"></div>
+      )}
       <footer className="text-white my-5">
         <a href="https://github.com/Cl84Dev" target="_blank">
           @Cl84Dev
